e69e0d4dd1efee6c9c66bd1146e65bda06e3479b
Courtney Owen <cowen@microsoft.com>
2019-02-07 14:02:11 -0800

    Modify project so it's a better canonical Angular sample
    - move taskpane.ts code to app.component.ts
    - move portion of taskpane.html into app.component.html
    - remove jquery packages

--- src/taskpane/app/app.component.ts
+++ src/taskpane/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as OfficeHelpers from "@microsoft/office-js-helpers";
 const template = require('./app.component.html');
 
 @Component({
@@ -9,6 +10,95 @@ export default class AppComponent {
     welcomeMessage = 'Welcome';
 
     async run() {
-        // Place code here.
+        switch (Office.context.host) {
+          case Office.HostType.Excel:
+            return this.runExcel();
+          case Office.HostType.OneNote:
+            return this.runOneNote();
+          case Office.HostType.Outlook:
+            return this.runOutlook();
+          case Office.HostType.PowerPoint:
+            return this.runPowerPoint();
+          case Office.HostType.Project:
+            return this.runProject();
+          case Office.HostType.Word:
+            return this.runWord();
+        }
+      }
+      
+      async runExcel() {
+        try {
+          await Excel.run(async context => {
+            /**
+             * Insert your Excel code here
+             */
+            const range = context.workbook.getSelectedRange();
+      
+            // Read the range address
+            range.load("address");
+      
+            // Update the fill color
+            range.format.fill.color = "yellow";
+      
+            await context.sync();
+            console.log(`The range address was ${range.address}.`);
+          });
+        } catch (error) {
+          OfficeHelpers.UI.notify(error);
+          OfficeHelpers.Utilities.log(error);
+        }
+      }
+      
+      async runOneNote() {
+        /**
+         * Insert your OneNote code here
+         */
+      }
+      
+      
+      async runOutlook() {
+        /**
+         * Insert your Outlook code here
+         */
+      }
+      
+      async runPowerPoint() {
+        /**
+         * Insert your PowerPoint code here
+         */
+        Office.context.document.setSelectedDataAsync("Hello World!",
+          {
+            coercionType: Office.CoercionType.Text
+          },
+          result => {
+            if (result.status === Office.AsyncResultStatus.Failed) {
+              console.error(result.error.message);
+            }
+          }
+        );
+      }
+      
+      async runProject() {
+        /**
+         * Insert your Outlook code here
+         */
+      }
+      
+      async runWord() {
+        return Word.run(async context => {
+          /**
+           * Insert your Word code here
+           */
+          const range = context.document.getSelection();
+      
+          // Read the range text
+          range.load("text");
+      
+          // Update font color
+          range.font.color = "red";
+      
+          await context.sync();
+          console.log(`The selected text was ${range.text}.`);
+        });
     }
 }
--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -2,108 +2,13 @@
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
+import 'zone.js'; // Required for Angular
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import AppModule from './app/app.module';
-import * as OfficeHelpers from "@microsoft/office-js-helpers";
 
-$(document).ready(() => {
-  $("#run").click(run);
-});
+Office.initialize = reason => {
+    document.getElementById('sideload-msg').style.display = 'none';
 
-// The initialize function must be run each time a new page is loaded
-Office.initialize = () => {
-  $("#sideload-msg").hide();
-  $("#app-body").show();
+    // Bootstrap the app
+    platformBrowserDynamic().bootstrapModule(AppModule).catch(error => console.error(error));
 };
-
-async function run() {
-  switch (Office.context.host) {
-    case Office.HostType.Excel:
-      return runExcel();
-    case Office.HostType.OneNote:
-      return runOneNote();
-    case Office.HostType.Outlook:
-      return runOutlook();
-    case Office.HostType.PowerPoint:
-      return runPowerPoint();
-    case Office.HostType.Project:
-      return runProject();
-    case Office.HostType.Word:
-      return runWord();
-  }
-}
-
-async function runExcel() {
-  try {
-    await Excel.run(async context => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
-
-      // Read the range address
-      range.load("address");
-
-      // Update the fill color
-      range.format.fill.color = "yellow";
-
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
-    });
-  } catch (error) {
-    OfficeHelpers.UI.notify(error);
-    OfficeHelpers.Utilities.log(error);
-  }
-}
-
-async function runOneNote() {
-  /**
-   * Insert your OneNote code here
-   */
-}
-
-
-async function runOutlook() {
-  /**
-   * Insert your Outlook code here
-   */
-}
-
-async function runPowerPoint() {
-  /**
-   * Insert your PowerPoint code here
-   */
-  Office.context.document.setSelectedDataAsync("Hello World!",
-    {
-      coercionType: Office.CoercionType.Text
-    },
-    result => {
-      if (result.status === Office.AsyncResultStatus.Failed) {
-        console.error(result.error.message);
-      }
-    }
-  );
-}
-
-async function runProject() {
-  /**
-   * Insert your Outlook code here
-   */
-}
-
-async function runWord() {
-  return Word.run(async context => {
-    /**
-     * Insert your Word code here
-     */
-    const range = context.document.getSelection();
-
-    // Read the range text
-    range.load("text");
-
-    // Update font color
-    range.font.color = "red";
-
-    await context.sync();
-    console.log(`The selected text was ${range.text}.`);
-  });
-}
