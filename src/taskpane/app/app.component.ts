7a073801bdd09e3d1b15807f00c91ca350b52903
Courtney Owen <38081557+TCourtneyOwen@users.noreply.github.com>
2019-02-07 09:01:08 -0800

    Additional changes to incorporate Angular into Office-Addin-Taskpane (#4)
    - Move app folder under taskpane folder
    - Remove sample code from app.component.ts
    - Import app.module into taskpane.ts

---
+++ src/taskpane/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+const template = require('./app.component.html');
+
+@Component({
+    selector: 'app-home',
+    template
+})
+export default class AppComponent {
+    welcomeMessage = 'Welcome';
+
+    async run() {
+        // Place code here.
+    }
+}
--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
-
+import AppModule from './app/app.module';
 import * as OfficeHelpers from "@microsoft/office-js-helpers";
 
 $(document).ready(() => {
