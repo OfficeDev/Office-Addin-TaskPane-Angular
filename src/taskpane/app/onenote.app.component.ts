b7922d8322e3cb53c1f6333a84f614674ec5edd6
Courtney Owen <cowen@microsoft.com>
2020-09-09 13:17:22 -0700

    Add "esModuleInterop": true, to tsconfig
    - Allows for normal import of images as we do in office-addin-taskpane and gets rid of the linting errors

--- src/taskpane/app/onenote.app.component.ts
+++ src/taskpane/app/onenote.app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from "@angular/core";
 // images references in the manifest
-const icon16 = require("../../../assets/icon-16.png");
-const icon32 = require("../../../assets/icon-32.png");
-const icon80 = require("../../../assets/icon-80.png");
+import "../../../assets/icon-16.png";
+import "../../../assets/icon-32.png";
+import "../../../assets/icon-80.png";
 const template = require("./app.component.html");
 /* global require */
 
