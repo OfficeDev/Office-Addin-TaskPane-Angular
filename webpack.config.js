04bee7a10b1f065a14019364c1dcb0742d0e1331
Darren Miller <darrmill@microsoft.com>
2021-12-14 13:50:12 -0800

    Merge shared changes

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,7 @@ module.exports = async (env, options) => {
       new CopyWebpackPlugin({
         patterns: [
           {
-            from: "assets/icon-*",
+            from: "assets/*",
             to: "assets/[name][ext][query]",
           },
           {
